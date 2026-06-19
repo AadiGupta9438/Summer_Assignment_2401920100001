ec049f03a1ac0d5e94155515d4731ecfba87c818
Aadi Gupta <aadigupta81206@gmail.com>
2026-06-19 18:13:36 +0530

    Week 3 Day 5 Solutions

---
+++ Week_3/Day_5/slidingwindowmaximum.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int arr[] = new int[n - k + 1];
+        Deque<Integer> deque = new LinkedList<>();
+        int j = 0;
+        for(int i = 0 ; i < n ; i++){
+            while(!deque.isEmpty() && deque.peekFirst() <= i - k){
+                deque.pollFirst();
+            }
+            while(!deque.isEmpty() && nums[deque.peekLast()] < nums[i]){
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            if(i >= k - 1){
+                arr[j++] = nums[deque.peekFirst()];
+            }
+        }
+        return arr;
+    }
+}
